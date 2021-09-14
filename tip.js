a3436655439fdd51b74334a06b61babca76ef03b
ryanjeffrey <ryanjeffreysmith@gmail.com>
2021-09-14 16:21:29 -0500

    Connected web3 library and converted ether to wei

--- tip.js
+++ tip.js
@@ -1,8 +1,22 @@
+const web3 = new Web3(Web3.givenProvider)
+
 const form = document.querySelector("form")
 
 const send = async function (amount) {
     const accounts = await window.ethereum.request({ method: "eth_requestAccounts" })
-    alert(accounts)
+    
+    const wei = web3.utils.toWei(amount, "ether")
+
+    if (accounts.length > 0) {
+        window.ethereum.request({
+          method: "eth_sendTransaction",
+          params: [{
+              from: accounts[0],
+              to: "0x6c0B013a01ef59EA0d8671c006E3365857A46187",
+              value: web3.utils.toHex(wei)
+            }],
+        });
+    }
 }
 
 if (window.ethereum) {
